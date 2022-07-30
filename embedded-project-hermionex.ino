c8c1440915b1f59d71961f5a1b7c8dbea4546598
grilona <107015090+grilona@users.noreply.github.com>
2022-07-30 12:09:30 +0300

    Update embedded-project-hermionex.ino

--- embedded-project-hermionex.ino
+++ embedded-project-hermionex.ino
@@ -2,12 +2,12 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
-#define BT_TX_PIN 4
+#define BT_TX_PIN 3
 #define BT_RX_PIN 2
 #define BT_PIN 13
 
-#define DIST_TRIG_PIN 2
-#define DIST_ECHO_PIN 4
+#define DIST_TRIG_PIN 6
+#define DIST_ECHO_PIN 7
 #define DIST_CONSTANT 0.017
 
 #define HEAT_PIN A0
@@ -15,8 +15,15 @@
 
 #define LIGHT_PIN A2
 
-#include "DHT.h"
-DHT dht(HEAT_PIN, DHT11);
+#define RED_LIGHT_PIN 11
+#define GREEN_LIGHT_PIN 10
+#define BLUE_LIGHT_PIN 9
+
+#define SERVO_PIN 4
+
+
+//#include "DHT.h"
+//DHT dht(HEAT_PIN, DHT11);
 SoftwareSerial BT(BT_TX_PIN, BT_RX_PIN); // TX, RX on arduino (RX, Tx on bluetooth)
 Servo myservo;
 
@@ -36,6 +43,15 @@ void setup()
 
   // Serial
   Serial.begin(9600);
+
+  // RGB led
+  pinMode(RED_LIGHT_PIN, OUTPUT);
+  pinMode(GREEN_LIGHT_PIN, OUTPUT);
+  pinMode(BLUE_LIGHT_PIN, OUTPUT);
+
+  //Servo
+   myservo.attach(SERVO_PIN);
+
 }
 
 // ========== DISTANCE ==========
@@ -58,11 +74,11 @@ float distanceSensor()
 
 // ========== HEAT ==========
 
-float heatSensor()
-{
-  return analogRead(HEAT_PIN) * HEAT_CONSTANT;
-  return dht.readTemperature();
-}
+//float heatSensor()
+//{
+//  return analogRead(HEAT_PIN) * HEAT_CONSTANT;
+//  return dht.readTemperature();
+//}
 
 // ========== LIGHT ==========
 
@@ -70,18 +86,30 @@ int lightSensor()
 {
   return analogRead(LIGHT_PIN);
 }
-
+// RGB colors
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(RED_LIGHT_PIN, red_light_value);
+  analogWrite(GREEN_LIGHT_PIN, green_light_value);
+  analogWrite(BLUE_LIGHT_PIN, blue_light_value);
+}
 // ========== MAIN ==========
 
 void loop()
 {
   char cmd;
   float distance = distanceSensor();
-  float temperature = heatSensor();
+//  float temperature = heatSensor();
   int light = lightSensor();
+//
 
-  //  Serial.print("Distance (cm): ");
-  //  distance >= 400 || distance <= 2 ? Serial.print("Out of range") : Serial.print(distance);
+    Serial.print("Distance (cm): ");
+    distance >= 400 || distance <= 2 ? Serial.print("Out of range") : Serial.print(distance)  ;
+
+  
+    distance <= 20 ? RGB_color(255, 0, 0) : RGB_color(0, 255, 0); // Red : Green
+ 
+
 
   if (BT.available())
   {
@@ -91,27 +119,27 @@ void loop()
 
     switch (cmd)
     {
-    case 1: // On
+    case '1': // On
       Serial.println("Executing case 1");
       myservo.write(180);
       break;
 
-    case 2: // Off
+    case '2': // Off
       Serial.println("Executing case 2");
       myservo.write(180);
       break;
 
-    case 3:
-      Serial.println("Executing case 3");
-      Serial.print(", temperature (C): ");
-      isnan(temperature) ? Serial.print("Error") : Serial.print(temperature);
+    case '3':
+//      Serial.println("Executing case 3");
+//      Serial.print(", temperature (C): ");
+//      isnan(temperature) ? Serial.print("Error") : Serial.print(temperature);
       break;
 
-    case 4:
+    case '4':
       Serial.println("Executing case 4");
       break;
 
-    case 5:
+    case '5':
       Serial.println("Executing case 5");
       break;
 
