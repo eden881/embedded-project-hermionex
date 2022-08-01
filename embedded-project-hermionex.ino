dfa17638279218df8daadaab5d9a9b414df64fd3
grilona <107015090+grilona@users.noreply.github.com>
2022-08-01 17:57:23 +0300

    Update embedded-project-hermionex.ino

--- embedded-project-hermionex.ino
+++ embedded-project-hermionex.ino
@@ -2,12 +2,12 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
-#define BT_TX_PIN 4
+#define BT_TX_PIN 3
 #define BT_RX_PIN 2
 #define BT_PIN 13
 
-#define DIST_TRIG_PIN 2
-#define DIST_ECHO_PIN 4
+#define DIST_TRIG_PIN 6
+#define DIST_ECHO_PIN 7
 #define DIST_CONSTANT 0.017
 
 #define HEAT_PIN A0
@@ -15,27 +15,201 @@
 
 #define LIGHT_PIN A2
 
-#include "DHT.h"
-DHT dht(HEAT_PIN, DHT11);
+#define RED_LIGHT_PIN 11
+#define GREEN_LIGHT_PIN 10
+#define BLUE_LIGHT_PIN 9
+
+#define SERVO_PIN 4
+
+#define  BUZZER_PIN 12
+
+
+//#include "DHT.h"
+//DHT dht(HEAT_PIN, DHT11);
 SoftwareSerial BT(BT_TX_PIN, BT_RX_PIN); // TX, RX on arduino (RX, Tx on bluetooth)
 Servo myservo;
 
+
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST 0
+
+// change this to make the song slower or faster
+int tempo = 144;
+
+// change this to whichever pin you want to use
+int buzzer = 12;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+
+  // Hedwig's theme fromn the Harry Potter Movies
+  // Socre from https://musescore.com/user/3811306/scores/4906610
+  
+  REST, 2, NOTE_D4, 4,
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4,
+  NOTE_G4, 2, NOTE_D5, 4,
+  NOTE_C5, -2, 
+  NOTE_A4, -2,
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4,
+  NOTE_F4, 2, NOTE_GS4, 4,
+  NOTE_D4, -1, 
+  NOTE_D4, 4,
+
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4, //10
+  NOTE_G4, 2, NOTE_D5, 4,
+  NOTE_F5, 2, NOTE_E5, 4,
+  NOTE_DS5, 2, NOTE_B4, 4,
+  NOTE_DS5, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_B4, 4,
+  NOTE_G4, -1,
+  NOTE_AS4, 4,
+     
+  NOTE_D5, 2, NOTE_AS4, 4,//18
+  NOTE_D5, 2, NOTE_AS4, 4,
+  NOTE_DS5, 2, NOTE_D5, 4,
+  NOTE_CS5, 2, NOTE_A4, 4,
+  NOTE_AS4, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_D4, 4,
+  NOTE_D5, -1, 
+  REST,4, NOTE_AS4,4,  
+
+  NOTE_D5, 2, NOTE_AS4, 4,//26
+  NOTE_D5, 2, NOTE_AS4, 4,
+  NOTE_F5, 2, NOTE_E5, 4,
+  NOTE_DS5, 2, NOTE_B4, 4,
+  NOTE_DS5, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_AS4, 4,
+  NOTE_G4, -1, 
+  
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
 void setup()
 {
   // Blutooth
-  pinMode(13, OUTPUT);
-  Serial.println("Hello");
+  pinMode(BT_PIN, INPUT_PULLUP);
+
   BT.begin(9600);
 
   // Distance sensor
   pinMode(DIST_TRIG_PIN, OUTPUT);
   pinMode(DIST_ECHO_PIN, INPUT);
-
-  // Heat sensor
-  pinMode(HEAT_PIN, INPUT);
-
+  
   // Serial
   Serial.begin(9600);
+
+  // RGB led
+  pinMode(RED_LIGHT_PIN, OUTPUT);
+  pinMode(GREEN_LIGHT_PIN, OUTPUT);
+  pinMode(BLUE_LIGHT_PIN, OUTPUT);
+
+  //Servo
+   myservo.attach(SERVO_PIN);
+
+  //Restart
+  Serial.begin(9600);
+  Serial.println("How to Reset Arduino Programmatically");
+
 }
 
 // ========== DISTANCE ==========
@@ -56,64 +230,95 @@ float distanceSensor()
   return duration * DIST_CONSTANT;
 }
 
-// ========== HEAT ==========
-
-float heatSensor()
-{
-  return analogRead(HEAT_PIN) * HEAT_CONSTANT;
-  return dht.readTemperature();
+// ========== RGB colors ==========
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(RED_LIGHT_PIN, red_light_value);
+  analogWrite(GREEN_LIGHT_PIN, green_light_value);
+  analogWrite(BLUE_LIGHT_PIN, blue_light_value);
 }
 
-// ========== LIGHT ==========
+// ========== BUZZER ==========
+void song(){
+ // iterate over the notes of the melody. 
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
 
-int lightSensor()
-{
-  return analogRead(LIGHT_PIN);
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration*0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+    
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
 }
-
 // ========== MAIN ==========
 
 void loop()
 {
+  myservo.write(0);
+
   char cmd;
   float distance = distanceSensor();
-  float temperature = heatSensor();
-  int light = lightSensor();
 
-  //  Serial.print("Distance (cm): ");
-  //  distance >= 400 || distance <= 2 ? Serial.print("Out of range") : Serial.print(distance);
+  distance <= 20 ? RGB_color(255, 0, 0) : RGB_color(0, 255, 0); // Red : Green
+  distance <= 20 ? tone(BUZZER_PIN, 900) : noTone(BUZZER_PIN); // Red : Green
+
+
+
 
   if (BT.available())
   {
 
     cmd = BT.read();
-    // Serial.println(cmd);
+     Serial.println(cmd);
 
     switch (cmd)
     {
-    case 1: // On
+    case '1': // On
       Serial.println("Executing case 1");
       myservo.write(180);
+      RGB_color(0, 255, 255); // Cyan
+
+      song();
+
+
       break;
 
-    case 2: // Off
-      Serial.println("Executing case 2");
-      myservo.write(180);
-      break;
-
-    case 3:
-      Serial.println("Executing case 3");
-      Serial.print(", temperature (C): ");
-      isnan(temperature) ? Serial.print("Error") : Serial.print(temperature);
-      break;
-
-    case 4:
-      Serial.println("Executing case 4");
-      break;
-
-    case 5:
-      Serial.println("Executing case 5");
-      break;
+//    case '2': // Off
+//    
+//      Serial.println("Executing case 2");
+//      myservo.write(0);
+//
+//
+//      break;
+//
+//    case '3':
+////      Serial.println("Executing case 3");
+////      Serial.print(", temperature (C): ");
+////      isnan(temperature) ? Serial.print("Error") : Serial.print(temperature);
+//      break;
+//
+//    case '4':
+//      Serial.println("Executing case 4");
+//      break;
+//
+//    case '5':
+//      Serial.println("Executing case 5");
+//      break;
 
     default:
       Serial.println("Executing default case");
